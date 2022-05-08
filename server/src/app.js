b1be9963ac6cbe4b639a0c52929eaa71ca1bf485
Meet Shah <montu48@gmail.com>
2022-05-08 14:23:57 +0530

    Added API Tests

--- server/src/app.js
+++ server/src/app.js
@@ -16,6 +16,9 @@ app.use(morgan('combined'))
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'..','public')))
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+  });
 
 app.use(planetsRouter)
 app.use("/launches",launchesRouter)
--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 const { parse } = require("csv-parse");
-const { httpGetAllPlanets } = require("../routes/planets/planets.controller");
 
 
 let habitabalPlanets = [];
