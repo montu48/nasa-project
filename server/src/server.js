8666655253faf6a39cd166012feb7d9743b65d54
Meet Shah <montu48@gmail.com>
2022-05-12 08:45:20 +0530

    Added MongoDB connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 5000;
 const app = require('./app');
 
 const { loadPlanetsData } = require('./models/planets.model');
 
+const MONGO_URL = "mongodb+srv://nasa-api:<password>@cluster0.3qyyx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+mongoose.connection.once('open',()=>{
+    console.log('connection is ready')
+})
+
+mongoose.connection.error('error',(err)=>{
+    console.error(err)
+})
 
 async function startServer() {
+    await mongoose.connect(MONGO_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
     await loadPlanetsData();
 
     app.listen(PORT, () => {
