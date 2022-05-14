16cab9756a3121b33c8bfc7caf14267b1ac38cff
Meet Shah <montu48@gmail.com>
2022-05-14 11:20:59 +0530

    Added connection to API with MongoDB

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,49 +1,91 @@
-// const launches = require('./launches.mongo');
+const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongoose');
 
 const launches = new Map();
-
-let latestFlightNumber = 100;
+const DEFAULT_FLIGHT_NUMBER = 100;
 
 const launch = {
-    flightNumber : 100,
-    mission : 'Keepler Exploration X',
-    rocket : 'Exploration IS1',
-    launchDate : new Date('March 28, 2022'),
-    target : 'Kepler-442 b',
-    customer : ['ZTM','NASA'],
-    upcoming : true,
+    flightNumber: 100,
+    mission: 'Keepler Exploration X',
+    rocket: 'Exploration IS1',
+    launchDate: new Date('March 28, 2022'),
+    target: 'Kepler-442 b',
+    customers: ['ZTM', 'NASA'],
+    upcoming: true,
     success: true,
 }
 
-launches.set(launch.flightNumber, launch);
-const existLaunchWithId = (launchId) =>{
-   return launches.has(launchId);
-}
-const getAllLaunches = ()=>{
-    return Array.from(launches.values());
+const saveLaunch = async (launch) => {
+    const planet = await planets.findOne({
+        keplerName: launch.target,
+    });
+    if (!planet) {
+        throw new Error('No matching planet found');
+    }
+    await launchesDatabase.findOneAndUpdate(
+        {
+            flightNumber: launch.flightNumber,
+        },
+        launch,
+        {
+            upsert: true,
+        }
+    );
+};
+
+saveLaunch(launch);
+
+const existLaunchWithId = async (launchId) => {
+    return await launchesDatabase.findOne({
+        flightNumber: launchId
+    })
 }
 
-const addNewLaunch = (launch) => {
-    latestFlightNumber++;
-    launches.set(latestFlightNumber,Object.assign(launch,{
-        success:true,
-        upcoming:true,
-        customers: ['Zero to Mastery','Nasa'],
-        flightNumber:latestFlightNumber
-    }))
+const getLatestFlightNumber = async () => {
+    const latestLaunch = await launchesDatabase.
+        findOne().
+        sort('-flightNumber')
+
+    if (!latestLaunch) {
+        return DEFAULT_FLIGHT_NUMBER
+    }
+
+    return latestLaunch.flightNumber;
 }
 
+const getAllLaunches = async () => {
+    return await launchesDatabase.find({}, {
+        '_id': 0, '__v': 0
+    })
+}
 
-const abortLaunchById = (launchId) =>{
-    const aborted =  launches.get(launchId);
-    aborted.upcoming = false;
-    aborted.success = false;
-    return aborted;
+const scheduleNewLaunch = async () => {
+    const newFlightNumber = await getLatestFlightNumber() + 1;
+    const newLaunch = Object.assign(launch, {
+        success: true,
+        upcoming: true,
+        customers: ['Zero to Mastery', 'Nasa'],
+        flightNumber: newFlightNumber
+    })
+
+    await saveLaunch(newLaunch)
+}
+
+const abortLaunchById = async (launchId) => {
+    const aborted = await launchesDatabase.updateOne({
+        flightNumber: launchId
+    }, {
+        upcoming: false,
+        success: false,
+    })
+
+    return aborted.modifiedCount === 1;
+
 }
 
 module.exports = {
     getAllLaunches,
-    addNewLaunch,
     existLaunchWithId,
-    abortLaunchById
+    abortLaunchById,
+    scheduleNewLaunch
 }
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,10 +1,10 @@
-const {getAllLaunches, addNewLaunch,existLaunchWithId, abortLaunchById} = require('../../models/launches.model');
+const {getAllLaunches, scheduleNewLaunch,existLaunchWithId, abortLaunchById} = require('../../models/launches.model');
 
 async function httpGetAllLaunches(req, res) {
-    return res.status(200).json(getAllLaunches());
+    return res.status(200).json(await getAllLaunches());
 }
 
-function httpAddNewLaunch(req, res){
+async function httpAddNewLaunch(req, res){
     let launch = req.body;
 
     if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.target){
@@ -18,21 +18,25 @@ function httpAddNewLaunch(req, res){
             error:"Invalid Launch Date"
         })
     }
-
-    addNewLaunch(launch);
+    await scheduleNewLaunch(launch);
     return res.status(201).json(launch)
 }
 
-function httpAbortLaunch(req,res) {
+async function httpAbortLaunch(req,res) {
     const launchId = +req.params.id;
-
-    if (!existLaunchWithId(launchId)) {
+    const existLaunch = await existLaunchWithId(launchId);
+    if (!existLaunch) {
         return res.status(404).json({
             error: 'Launch Not Found'
         })
     }
-    const aborted = abortLaunchById(launchId);
-    return res.status(200).json(aborted);
+    const aborted = await abortLaunchById(launchId);
+    if(!aborted){
+        return res.status(400).json({
+            error:'Launch not aborted'
+        });    
+    }
+    return res.status(200).json({ok:true});
     
 }
 
